4f1b9c07d959d07847e0f662f337fb0f65f70c0f
Santeri Moberg <santeri.moberg30@gmail.com>
2020-01-24 14:53:02 +0200

    Lab 2

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.example.lab1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var fabOpened = false
+
+        fab.setOnClickListener{
+
+            if (!fabOpened){
+
+                fabOpened = true
+
+                fab_map.animate().translationY(-resources.getDimension(R.dimen.standard_66))
+                fab_time.animate().translationY(-resources.getDimension(R.dimen.standard_116))
+
+            } else{
+
+                fabOpened = false
+
+                fab_map.animate().translationY(0f)
+                fab_time.animate().translationY(0f)
+
+            }
+        }
+
+        fab_time.setOnClickListener{
+
+            startActivity(Intent(applicationContext, TimeActivity::class.java))
+        }
+
+        fab_map.setOnClickListener{
+            startActivity(Intent(applicationContext, MapActivity::class.java))
+        }
     }
 }
